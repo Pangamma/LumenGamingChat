31d637fcc9e78b0ffb0d770871e6ca08dc5cc7da
Taylor Love <Taylor Love>
2016-12-12 10:19:11 -0800

    Add non-thread-safe temporary testing code Remove libraries from dist folder.

--- src/com/lumengaming/chat/spigot/listeners/ChatListener.java
+++ src/com/lumengaming/chat/spigot/listeners/ChatListener.java
@@ -1,7 +1,9 @@
 package com.lumengaming.chat.spigot.listeners;
 import com.lumengaming.chat.spigot.LumenGamingMessageEvent;
 import com.lumengaming.chat.spigot.Main;
+import java.util.LinkedList;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -9,12 +11,17 @@ import org.bukkit.event.player.AsyncPlayerChatEvent;
 
 public class ChatListener implements Listener{
     private final Main plugin;
-    
+    private LinkedList<String> listeners = new LinkedList<String>();
     public ChatListener(Main plugin){
         this.plugin = plugin;
     }
     @EventHandler(ignoreCancelled = true, priority = EventPriority.NORMAL)
     public void onSpigotMessage(AsyncPlayerChatEvent e){
+        if (e.getMessage().equals("LGCTEST=1")){
+            listeners.add(e.getPlayer().getName());
+        }else if (e.getMessage().equals("LGCTEST=0")){
+            listeners.remove(e.getPlayer().getName());
+        }
         plugin.getClient().send("AsyncChat=>"+e.getMessage());
 //        if (this.plugin.getMiniPlugin().getFactory().getChannel("LumenChat").getOutgoingChannel().send(e, null)
 //            JsonObject obj = new JsonObject();
@@ -30,6 +37,10 @@ public class ChatListener implements Listener{
     
     @EventHandler
     public void onProxyMessage(LumenGamingMessageEvent e){
-        Bukkit.broadcastMessage("§aGOT:"+new String(e.getData()));
+        for(Player p : Bukkit.getOnlinePlayers()){
+            if (p.isOp() || listeners.contains(p.getName())){
+                p.sendMessage("§aGOT:"+new String(e.getData()));
+            }
+        }
     }
 }
